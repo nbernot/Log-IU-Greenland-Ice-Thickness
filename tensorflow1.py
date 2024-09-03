ecec6767e6f865c4ff0319728d59aff1f29d0fcf
nbernot <165718024+nbernot@users.noreply.github.com>
2024-09-03 12:20:07 -0400

    Including a testing model I found online that shows that tensorflow is working on your device

--- tensorflow1.py
+++ tensorflow1.py
@@ -1,2 +1,23 @@
-import Tensorflow as tf
-#hello
+import tensorflow as tf
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, activation='relu', input_shape=(3,)),
+    tf.keras.layers.Dense(1)
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Print model summary
+model.summary()
+
+# Create dummy data
+import numpy as np
+x = np.random.random((5, 3))
+y = np.random.random((5, 1))
+
+# Train the model
+model.fit(x, y, epochs=1)
+
+# Make predictions
+predictions = model.predict(x)
+print("Predictions:", predictions)
